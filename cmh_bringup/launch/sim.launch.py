7a4c8dcb7346f1f8454c13f11590627d133b913b
Fourat Zai El Amri <fourat.amri@hotmail.com>
2026-04-07 14:27:52 +0200

    integrate Ackermann steering, added ros bridge,fixed bugs with the physics

--- cmh_bringup/launch/sim.launch.py
+++ cmh_bringup/launch/sim.launch.py
@@ -2,7 +2,8 @@ import os
 import csv
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess,AppendEnvironmentVariable
+from launch.actions import ExecuteProcess, AppendEnvironmentVariable, DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 import xacro
 
@@ -17,21 +18,18 @@ def get_car_start(csv_path):
                     x, y, yaw = row[1], row[2], row[3]
                     break
     return x, y, yaw
-
-def generate_launch_description():
-
-    track_name=str(input("please specify the track name: "))
-    # 1. Paths to your packages
+def launch_setup(context, *args, **kwargs):
+    
+    track_name = LaunchConfiguration('track_name').perform(context)
+    # 1. Paths to packages
     pkg_gazebo = get_package_share_directory('cmh_gazebo')
     pkg_desc = get_package_share_directory('cmh_description')
-
-    track_csv = os.path.join(pkg_gazebo, 'csv', track_name +'.csv')
+    track_csv = os.path.join(pkg_gazebo, 'csv', track_name + '.csv')
     car_x, car_y, car_yaw = get_car_start(track_csv)
+    # 2. Path to the world file
+    world_file = os.path.join(pkg_gazebo, 'worlds', track_name + '.sdf')
 
-    # 2. Path to the world file (using the one you manually migrated)
-    world_file = os.path.join(pkg_gazebo, 'worlds', track_name +'.sdf')
-
-    # 3. Start Gazebo Harmonic with your track
+    # 3. Start Gazebo Harmonic with the track
     gazebo = ExecuteProcess(
         cmd=['gz', 'sim', '-r', world_file],
         output='screen'
@@ -39,7 +37,7 @@ def generate_launch_description():
 
     # 4. Process the URDF/Xacro file and pass the config YAML
     xacro_file = os.path.join(pkg_desc, 'urdf', 'car.urdf.xacro')
-    yaml_config_file = os.path.join(pkg_desc, 'urdf', 'configDry.yaml')
+    yaml_config_file = os.path.join(pkg_desc, 'config', 'configDry.yaml')
     
     # Pass the config_file argument into the mappings dictionary
     robot_description_config = xacro.process_file(xacro_file, mappings={'config_file': yaml_config_file})
@@ -65,9 +63,35 @@ def generate_launch_description():
     model_path = os.path.join(pkg_gazebo, 'models') + ':' + share_dir
     set_env = AppendEnvironmentVariable('GZ_SIM_RESOURCE_PATH', model_path)
 
+    # We must also start the Robot State Publisher so ROS 2 knows the car's joints
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': urdf, 'use_sim_time': True}]
+    )
+
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            # Pass the simulation clock from Gazebo to ROS 2
+            '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
+            # Pass velocity commands from ROS 2 to Gazebo
+            '/cmd_vel@geometry_msgs/msg/Twist]gz.msgs.Twist',
+        ],
+        output='screen'
+    )
+
+    # Return the nodes you want to launch
+    return [set_env, gazebo, spawn_entity, bridge, robot_state_publisher]
+
+def generate_launch_description():
+    track_arg = DeclareLaunchArgument(
+        'track_name',
+        default_value='small_track',
+        description='Name of the track to launch'
+    )
     return LaunchDescription([
-        set_env,
-        gazebo,
-        spawn_entity,
-        
+        track_arg,
+        OpaqueFunction(function=launch_setup)
     ])
